ca75b1ec978946673e162fe8bd656eb23c7d79e0
Eric Wilson <eric.wilson@geekcafe.com>
2021-03-21 19:28:44 -0400

    🧹 clean (comments)

--- scripts/server-configurations/aws/ec2-config/ec2-install-ssh-key.sh
+++ scripts/server-configurations/aws/ec2-config/ec2-install-ssh-key.sh
@@ -1,10 +1,16 @@
-
-
-
 # warning this will replace the keys with what you have here
+# so you should get a copy of the public key generated from the
+# pem key that AWS generates.  To do this simply start a new EC2
+# instance. Navigate to the authorized_keys directory and copy the
+# ssh key. Then add it to this section along with any other keys you
+# want to have access to your server
 sudo cat -s > ~/.ssh/authorized_keys << EOF
-# add your key(s) here
+# add your public key(s) here
 EOF
 
-# or to just append it
-sudo echo "ENTER_YOUR_KEY_HERE" >> ~/.ssh/authorized_keys
+
+# option 2 append a key
+sudo echo "ENTER_YOUR_PUBLIC_KEY_HERE" >> ~/.ssh/authorized_keys
+
+# do you need to generate a key use
+# 
